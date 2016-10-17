4c2f4836ade8bf98a8f8ad2817de99a1885766a8
Mirko Vaars <mirko.vaars@gmail.com>
2016-10-17 20:50:52 +0200

    html added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || '1337';
+
+app.use(
+	express.static(
+		path.resolve(__dirname, '..', 'public')
+	)
+);
 
 app.get('/', (req, res) => res.end('Hoooooi'));
 
