2b810f25c58db52e78cfc009a2b13026c85a9030
Mirko Vaars <mirko.vaars@gmail.com>
2016-10-17 20:34:22 +0200

    added babel

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || '1337';
+
+app.get('/', (req, res) => res.end('Hi'));
+
+app.listen(port, host, (err) => {
+	if(err) console.log('Error error, does not compute');
+	console.log('Server started at %s:%d', host, port);
+});
