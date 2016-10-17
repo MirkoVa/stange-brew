5b68027d63d80c7de6b5ed112daf5258da0a79dd
Mirko Vaars <mirko.vaars@gmail.com>
2016-10-17 20:43:52 +0200

    added nodemon

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || '1337';
 
-app.get('/', (req, res) => res.end('Hi'));
+app.get('/', (req, res) => res.end('Hoooooi'));
 
 app.listen(port, host, (err) => {
 	if(err) console.log('Error error, does not compute');
